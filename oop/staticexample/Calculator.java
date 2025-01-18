92047db2fa0f7773cbf73a183928dc7e67e6a385
adelya <adisen24@yandex.ru>
2025-01-18 13:25:45 -0800

    oop

--- oop/staticexample/Calculator.java
+++ oop/staticexample/Calculator.java
@@ -1,10 +1,22 @@
 package eighthtask.oop.staticexample;
 
 public class Calculator {
+//    double a;
+//    double b;
+//
+//    public Calculator(double a, double b) {
+//        this.a = a;
+//        this.b = b;
+//    }
+//
+//    public double sum(double c) {
+//        return a + b + c;
+//    }
 
 //    пример явного преобразования (сужающее преобразование)
     public static int sum(double a, double b) {
         return (int) (a + b);
+//        System.out.println(a+b);
     }
 
 //    пример явного преобразования (сужающее преобразование)
@@ -23,8 +35,7 @@ public class Calculator {
 
     public static double divide(double a, double b) {
         if (b < 0) {
-//            ArithmeticException: исключение, возникающее при делении на ноль
-            throw new ArithmeticException("Деление на ноль");
+            throw new IllegalArgumentException("Деление на ноль");
         }
         return a/b;
     }
